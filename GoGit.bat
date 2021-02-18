7635fd21b805c83f033dc00a80f94e2fbe2275b0
Colgate13x <gabreilbarros13@gmail.com>
2021-02-18 13:33:34 -0300

    Push by GoGit ->

--- GoGit.bat
+++ GoGit.bat
@@ -5,7 +5,7 @@ git add app.json
 git add package.json
 git add yarn.lock
 git add assets
-git commit -m "Push by GoGi ->"
+git commit -m "Push by GoGit ->"
 git pull https://github.com/Colgate13/AppPortifolio main
 git push https://github.com/Colgate13/AppPortifolio -u main
 git status
