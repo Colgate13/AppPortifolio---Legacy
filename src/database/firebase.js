b3a9512868123bbc3f60706342d86e2b808ad20e
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2021-08-25 10:26:37 -0300

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import "firebase/firestore";
 
 const firebaseapp = firebase.initializeApp({
-    apiKey: "AIzaSyDoZ7_IW4g8FMeTnvDO4W_KrCpvI9sTOWw",
-    authDomain: "portifolioapp-d5f03.firebaseapp.com",
-    projectId: "portifolioapp-d5f03",
-    storageBucket: "portifolioapp-d5f03.appspot.com",
-    messagingSenderId: "851782185438",
-    appId: "1:851782185438:web:7ebfcc88b211296029ed4f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 const db = firebase.firestore();
